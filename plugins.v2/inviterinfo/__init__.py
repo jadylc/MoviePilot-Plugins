83ffa69c490f5583674d49d68ec79ced7580cca3
jadylc <jadylc@example.com>
2025-12-16 17:30:49 +0800

    数据存储

--- plugins.v2/inviterinfo/__init__.py
+++ plugins.v2/inviterinfo/__init__.py
@@ -32,7 +32,7 @@ class InviterInfo(_PluginBase):
     # 插件图标
     plugin_icon = "user.png"
     # 插件版本
-    plugin_version = "1.21"
+    plugin_version = "1.22"
     # 插件作者
     plugin_author = "MoviePilot"
     # 作者主页
